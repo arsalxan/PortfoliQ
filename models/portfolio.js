0836c6b992737370d57caaa24fca7ec8904bec65
arsalxan <thearsalansiddiqui@gmail.com>
2025-10-17 14:17:39 +0530

    minor console bug fixed

--- models/portfolio.js
+++ models/portfolio.js
@@ -21,7 +21,6 @@ const portfolioSchema = new mongoose.Schema({
 portfolioSchema.post('findOneAndDelete', async function (doc) {
   if (doc) {
     await Feedback.deleteMany({ portfolio: doc._id });
-    console.log(res);
   }
 });
 
